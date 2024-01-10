627afda6f13e248d24120fa02edfdc29305d7b7c
ThanhNTH <thanhnh.hpvn@gmail.com>
2024-01-10 16:16:01 +0700

    Initial Commit

---
+++ linkid_ios_sdk/linkid_ios_sdk.h
@@ -0,0 +1,18 @@
+//
+//  linkid_ios_sdk.h
+//  linkid_ios_sdk
+//
+//  Created by ThanhNTH on 10/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for linkid_ios_sdk.
+FOUNDATION_EXPORT double linkid_ios_sdkVersionNumber;
+
+//! Project version string for linkid_ios_sdk.
+FOUNDATION_EXPORT const unsigned char linkid_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <linkid_ios_sdk/PublicHeader.h>
+
+
